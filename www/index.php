0c972dd4706c39982d5f60631d560cf603b2d481
phanupon <phanupon.p@gmail.com>
2026-02-26 10:01:33 +0700

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,3 +1,11 @@
-<?php
-echo "hello";
-?>
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
